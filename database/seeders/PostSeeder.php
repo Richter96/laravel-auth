c23e9cd462ee7897aefb97bf9703fd3937272aee
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-05 15:57:09 +0200

    add fake data

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
 
 class PostSeeder extends Seeder
 {
@@ -12,8 +16,16 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+
+            $post = new Post();
+            $post->title = $faker->sentence(4);
+            $post->slug = Str::slug($post->title, '-');
+            $post->text = $faker->paragraphs(asText: true);
+            $post->image = $faker->imageUrl(category: 'Posts', format: 'jpg');
+            $post->save();
+        }
     }
 }
