02468c4aacb2fa4dd371e8834fc2d3927f461b8e
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-05 17:45:24 +0200

    add file crud

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+
+    @if (session('message'))
+    <div class="alert alert-primary" role="alert">
+        <strong>{{session('message')}}</strong>
+    </div>
+
+    @endif
+
+    <h1>POSTS</h1>
+
+
+    <a class="btn btn-primary my-3" href="{{route('admin.posts.create')}}" role="button">Create new post</a>
+
+    <div class="table-responsive">
+        <table class="table table-secondary">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">IMAGE</th>
+                    <th scope="col">TITLE</th>
+                    <th scope="col">SLUG</th>
+                    <th scope="col">DESCRIPTION</th>
+                    <th class="col-2" scope="col">ACTION</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                @forelse ($posts as $post)
+                <tr>
+                    <td scope="row">{{$post->id}}</td>
+                    <td>
+                        <img width="100" class="img-fluid" src="" alt="{{$post->title}}">
+                    </td>
+
+                    <td>{{$post->title}}</td>
+                    <td>{{$post->slug}}</td>
+                    <td>{{$post->text}}</td>
+                    <td>
+                        <div class="row row-cols-3">
+
+                            <div class="icon d-inline">
+                                <a name="" id="" class="bg-violet-500 btn btn-primary w-1rem" href="{{ Route('admin.posts.show', $post->id) }}" role="button"><i class="fas fa-eye fa-sm fa-fw"></i></a>
+                            </div>
+
+                            <div class="icon d-inline">
+                                <a name="" id="" class="bg-violet-500 btn btn-primary w-1rem" href="{{ Route('admin.posts.edit', $post->id) }}" role="button"><i class="fas fa-pencil fa-sm fa-fw"></i></a>
+                            </div>
+                            <div class="icon d-inline">
+                                <form action="{{route('admin.posts.destroy', $post->id)}}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger"><i class="fas fa-trash fa-sm fa-fw w-50"></i>
+                                    </button>
+                                </form>
+                            </div>
+
+                    </td>
+
+                </tr>
+
+                @empty
+                <tr class="">
+                    <td>No results</td>
+                </tr>
+                @endforelse
+
+
+            </tbody>
+        </table>
+    </div>
+
+</div>
+
+
+
+@endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -22,17 +22,57 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-            <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">BoolPress</a>
-            <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-            <ul class="navbar-nav px-3">
-                <li class="nav-item text-nowrap">
-                    <a class="nav-link" href="#">Sign out</a>
-                </li>
-            </ul>
+
+        <nav class="navbar navbar-expand-md navbar bg-dark border-bottom border-bottom-dark shadow-sm" data-bs-theme="dark">
+            <div class="container">
+                <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
+                        </li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+                        @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('admin.dashboard') }}">{{__('Dashboard')}}</a>
+                                <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
         </nav>
 
         <div class="container-fluid vh-100">
