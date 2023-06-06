ab7a9c300223d9d3d52bf72fc112bf03a975d891
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-06 09:53:26 +0200

    insert function slug and protect fillavle

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'slug', 'image', 'text'];
+
+    public static function generateSlug($title)
+    {
+        return Str::slug($title, '-');
+    }
 }
